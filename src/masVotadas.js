4fff3adb75ebcfbaf968fb6a8ee41c2a432130de
Lautaro Valdez <57192096+Tabitoo@users.noreply.github.com>
2020-11-18 19:59:56 -0300

    Modificacion de mas votadas, se agrego promedio y solucion de errores

--- src/masVotadas.js
+++ src/masVotadas.js
@@ -1,21 +1,28 @@
 const fs = require('fs')
 
-let votadas = {
-    bd : '/data/movies.json',
+module.exports = {
+    bd : '../data/movies.json',
     leerJSON : function() {
-        let  movies = JSON.parse(fs.readFile(this.bd, 'utf-8'))
-        return movies
+        return JSON.parse(fs.readFileSync(this.bd,'utf-8'));
+        
     },
      ranking : function() {
-       let movies = this.leerJSON();
-       movies = movies.filter(function(movies) {
-           return movies.vote_average > 7
-       })
-       return movies
+       let movies = this.leerJSON().movies;
+       return movies.filter(peliculas => peliculas.vote_average >= 7)
+       
+       
     },
      totalPelis : function() {
          return this.ranking().length
 
+     },
+     promedio : function() {
+         let totalPelis = this.totalPelis()
+         let pelis = this.ranking()
+         let num = 0
+        pelis.forEach(function(peli){
+            num = num + peli.vote_average
+        });
+        return num / totalPelis
      }
 }
-
