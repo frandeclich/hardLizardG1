4f64a8b6c5e1ba5952e4add63904115fdf5101d0
frandeclich <fdeclich1@gmail.com>
2020-11-18 18:50:12 -0300

    contacto primera versión

--- src/contacto.js
+++ src/contacto.js
@@ -1,5 +1,5 @@
 //A ver...
-const fs=require('fs')
+const fs=require('fs') 
 let contacto={
     titulo:'Contáctanos.',
     contenido:' ¿Tenés algo para contarnos? Nos encanta escuchar a nuestros clientes. Si deseas contactarnos podés escribirnos al siguiente email:dhmovies@digitalhouse.com o en las redes sociales. Envianos tu consulta, sugerencia o reclamo y será respondido a la brevedad posible. Recordá que también podes consultar la sección de Preguntas Frecuentes para obtener respuestas inmediatas a los problemas más comunes.'
