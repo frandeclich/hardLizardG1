2018ec3bac59c99653b708e1f8b4f5167dc6b13f
frandeclich <fdeclich1@gmail.com>
2020-11-18 13:31:52 -0300

    En Cartelera listo

---
+++ src/enCartelera.js
@@ -0,0 +1,27 @@
+const fs = require('fs')
+
+carteleraData={
+    titulo:"En Cartelera.",
+    bd:'./data/movies.json',
+    leerJSON:function(){
+        return JSON.parse(fs.readFileSync(this.bd,'utf-8'))
+    },
+    numeroPeliculas:function(){
+        return this.leerJSON().total_movies
+    },
+    titulos:function(){
+        arrayT=[]
+        this.leerJSON().movies.forEach(movie => {
+            arrayT.push(movie.title)
+        });
+        return arrayT
+    },
+    reseñas:function(){
+        arrayR=[]
+        this.leerJSON().movies.forEach(movie => {
+            arrayR.push(movie.overview)
+        });
+        return arrayR
+    }
+}
+console.log(carteleraData.reseñas())
