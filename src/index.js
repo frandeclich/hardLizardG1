fa51a0dd7893245014c1cea9aaf36dde67157b40
frandeclich <fdeclich1@gmail.com>
2020-11-18 19:39:12 -0300

    Primera versión del index

--- src/index.js
+++ src/index.js
@@ -1,16 +1,33 @@
-let homePage = require('./homePage')
+let homePage = require('./homePage');
+let enCartelera = require('./enCartelera');
+let masVotadas = require('./masVotadas');
+let sucursales = require('./sucursales');
+let contacto = require('./contacto');
+let preguntasFrecuentes = require('./preguntasFrecuentes');
 
-let movies = homePage.leerJSON()
-
-module.exports = {
-    homePage : function(req,res){
-        movies.movies.forEach(movie => {
-            res.write(movie.title +'\n')
-        });
+let index = {
+    homePage: function(res) {
+        res.write(homePage.titulo)
+        res.write('\n')
+        res.write('\n\n')
+        res.write('Total de peliculas en cartelera: ' + homePage.totalPelis())
+        res.write('\n\n')
+        res.write('--------------------------\n')
+        res.write('Listado de Peliculas disponibles\n').toUpperCase()
+        res.write('--------------------------')
+        res.write('\n\n')
         
-        res.end()
-    },
-    enCartelera : function(req,res){
+        let titleMovies = homePage.listadoTitulos();
+        for (title of titleMovies) {
+            res.write(title)
+            res.write('\n')
 
-    }
-}
+        } 
+        res.write('\n')
+        res.end('Recorda que podes visitar las secciones:
+        i. En Cartelera
+        ii. Mas Votadas
+        iii. Sucursales
+        iv. Contacto
+        v. Preguntas Frecuentes ')
+    },
