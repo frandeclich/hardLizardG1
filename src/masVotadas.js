583e64005d70ae1469b5069d24e7daf3e57b9870
frandeclich <fdeclich1@gmail.com>
2020-11-19 01:36:08 -0300

    Arreglos y le agregué titulo.

--- src/masVotadas.js
+++ src/masVotadas.js
@@ -4,26 +4,23 @@ module.exports = {
     bd : '../data/movies.json',
     titulo: 'Más Votadas.',
     leerJSON : function() {
-        return JSON.parse(fs.readFileSync(this.bd,'utf-8'));
-        
+        return JSON.parse(fs.readFileSync(this.bd,'utf-8'));        
     },
-     ranking : function() {
-       let movies = this.leerJSON().movies;
-       return movies.filter(peliculas => peliculas.vote_average >= 7)
-       
-       
+    ranking : function() {
+        let movies = this.leerJSON().movies;
+        return movies.filter(peliculas => peliculas.vote_average >= 7)       
     },
-     totalPelis : function() {
-         return this.ranking().length
-
-     },
-     promedio : function() {
+    totalPelis : function() {
+        return this.ranking().length
+    },
+    promedio : function() {
          let totalPelis = this.totalPelis()
          let pelis = this.ranking()
          let num = 0
         pelis.forEach(function(peli){
             num = num + peli.vote_average
         });
-        return num / totalPelis
-     }
+        return (num / totalPelis).toFixed(2)
+        
+    }
 }
